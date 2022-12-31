932c8a62abc0652417f03d298dd32c6cfafb9457
Sunwin Sunny <92300722+DovakhinX@users.noreply.github.com>
2022-12-31 13:20:02 +0700

    Add files via upload

---
+++ src/Components/Element/Element.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Element.css";
+const Element = () => {
+  return (
+    <div className="contain">
+      <button onClick={() => alert("Lords Grace")}>tree</button>
+    </div>
+  );
+};
+
+export default Element;
