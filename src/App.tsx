6c1a0d59d34a4d23def2eaf274cdd8295efe789b
Sunwin Sunny <92300722+DovakhinX@users.noreply.github.com>
2022-12-31 13:24:25 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Card1 from "./Components/Card1/Card1";
-import Card2 from "./Components/Card2/Card2";
 
+import Element from "./Components/Element/Element";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  function Finisher() {
+    alert("You are done here!!");
+  }
 
   return (
     <>
-    <Navbar />
-    <div className="container">
-      <div className="card">
-        <p className="pak">scratchcard-react </p>
-        <div className="carding">
-          <Card1/>
+      <Navbar />
+      <div className="container">
+        <div className="card">
+          <p className="pak">Scratch Me!! </p>
+          <div className="carding">
+            <Card1
+              dimension={300}
+              percent={60}
+              element={<Element />}
+              finisher={Finisher}
+              radius={20}
+            />
+          </div>
         </div>
       </div>
-      <div className="card">
-      <p className="pak">coming soon </p>
-      <div className="carding">
-          <Card2 />
-        </div>
-
-
-      </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
