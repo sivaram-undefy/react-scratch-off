32cccdf53f452bbfbbc438d66813ab939676d160
DovakhinX <sunwin60@gmail.com>
2023-01-13 17:00:21 +0530

    Type update

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
               key={item}
               dimension={300}
               percent={60}
-              element={<Element data={`You have won $${item}`} />}
+              element={<Element data={`You have won $${item+1}`} />}
               onDone={onDone}
               radius={20}
             />
